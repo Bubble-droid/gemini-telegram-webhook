aafc6c752b89d0bf58866719af9d997c607cc28d
Bubble-droid <Bubble-droid@github.com>
2025-09-17 14:05:03 +0800

    Update

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -21,5 +21,8 @@ echo "sing-box is up!"
 
 echo "nameserver 127.0.0.1" >| /etc/resolv.conf
 
+echo "precedence ::ffff:0:0/96 0" >> /etc/gai.conf \
+    && echo "precedence ::/0 50" >> /etc/gai.conf
+
 # 将 PID1 换成 proxychains4 node（proxychains4 位于 PATH）
 exec proxychains4 node --enable-source-maps /app/dist/index.js
